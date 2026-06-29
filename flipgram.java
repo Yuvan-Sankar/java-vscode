462c764a3df5e702b85694e9c037172ba22044b7
Yuvan-Sankar <yuvansankarnyc@gmail.com>
2026-06-29 09:57:47 -0400

    added file that checks if a word is a heterogram or not. A heterogram is a word, phrase, or sentence in which no letter of the alphabet occurs more than once. it then flips the word if it does not fit the definition of a heterogram.

---
+++ flipgram.java
@@ -0,0 +1,47 @@
+import java.util.*;
+public class flipgram {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a word: ");
+        String input = sc.next().toUpperCase();
+        char flipgram[] = new char[26];
+        boolean isFlip = false;
+        for(int i=0; i<input.length(); i++) {
+            char c = input.charAt(i);
+            for(int j=0; j<flipgram.length; j++) {
+                if(c == flipgram[j]) {
+                    isFlip = true;
+                    break;
+                    
+                }
+            }
+            if(!isFlip) {
+                flipgram[i] = c;
+            }
+        }
+        if(isFlip==false){
+            System.out.println("HETEROGRAM");
+            System.exit(0);
+        }
+        if(input.length()%2==0){
+            String first=input.substring(0, input.length()/2);
+            String second=input.substring(input.length()/2, input.length());
+            System.out.println(second+first);
+        }
+        else{
+            String first=input.substring(0, input.length()/2);
+            String second=input.substring(input.length()/2+1, input.length());
+            String middle=input.substring(input.length()/2, input.length()/2+1);
+            System.out.println(second+middle+first);
+        }
+            
+
+    }
+}
+/*
+1. take input
+2. check if letter is a heterogram, if it is, display heterogram
+3. check if length of the string is even or odd
+4. if even, split the string into two halves and reverse each half
+5. if odd, split the string into two halves, reverse each half and keep the middle character
+*/
